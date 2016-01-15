54d58bf974fdde126f222c79d3cba5cbc8a1d825
bugulin <petriczech12@gmail.com>
2016-01-15 22:52:18 +0100

    Improving gauss.py

--- gauss.py
+++ gauss.py
@@ -4,33 +4,50 @@ class Matrix:
 
     def run(self):
         self.vars = len(self.matrix)
-        for i in range(self.vars-1):
-            a = self.matrix[i][i]
-            b = self.matrix[i+1][i]
-            for n in range(self.vars+1):
-                self.matrix[i+1][n] *= a
-            for n in range(self.vars+1):
-                self.matrix[i][n] *= b
-            for n in range(self.vars+1):
-                self.matrix[i+1][n] -= self.matrix[i][n]
-
-        print(self)
+        self.matrix = sorted(self.matrix)
+        
+        for y in range(self.vars-1):
+            for i in range(self.vars-1-y):
+                if self.matrix[i][y]:
+                    b = self.matrix[i+1].copy()
+                    bi, ai = self.n(self.matrix[i][y], b[y])
+                    
+                    for n in range(self.vars+1):
+                        b[n] *= ai
+                    for n in range(self.vars+1):
+                        self.matrix[i][n] *= bi
+                    for n in range(self.vars+1):
+                        self.matrix[i][n] -= b[n]
+            #print(self)
 
         result = [0]*self.vars
-        for i in range(0, self.vars)[::-1]:
+        c = self.vars-1
+        for i in range(0, self.vars):
             a = self.matrix[i]
-            print(a)
-            for n in range(i+1, self.vars):
-                print("-", a[n])
-                a[self.vars] -= a[n]*result[n]
+            #print(a)
+            for n in range(self.vars-i, self.vars):
+                #print("-", a[n])
+                a[self.vars] -= a[n]*result[c-n]
                 a[n] = 0
-            result[i] = a[self.vars]/a[i]
+            result[i] = a[self.vars]/a[c-i]
 
-        #result.insert(0, (self.matrix[-2][-1] - self.matrix[-2][-2]*result[0]) / self.matrix[-2][-3])
-        return result
+        chars = "xyzabcdefghijklmno"
+        for i in range(self.vars):
+            print(chars[i], "=", result[i], end=", ")
+        print()
+        return result[::-1]
 
-    def eliminate(self, i, n):
-        pass
+    def n(self, a, b):
+        abs_a = abs(a)
+        abs_b = abs(b)
+        multiple_of_a = abs(a)
+        multiple_of_b = abs(b)
+        while multiple_of_a != multiple_of_b:
+            if multiple_of_a > multiple_of_b:
+                multiple_of_b += abs_b
+            elif multiple_of_b > multiple_of_a:
+                multiple_of_a += abs_a
+        return multiple_of_a/a, multiple_of_b/b
         
     def __str__(self):
         output = ""
@@ -55,9 +72,10 @@ class Matrix:
         return output
 
 a = Matrix()
-#a.matrix.append([2, 3, 7, 47])
-#a.matrix.append([3, 8, 1, 50])
-#a.matrix.append([0, 3, 3, 27])
+a.matrix.append([2, 3, 7, 47])
+a.matrix.append([3, 8, 1, 50])
+a.matrix.append([0, 3, 3, 27])
 
-a.matrix.append([4, 1, 5])
-a.matrix.append([12, 3, 15])
+#a.matrix.append([8, 80, 7, 155])
+#a.matrix.append([10, 30, 5, 30])
+#a.matrix.append([31, 11, 5, 0])
